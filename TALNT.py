d6f7a8cd981245ec14c64944779f6413226d5c79
Lienid <0xLienid@protonmail.com>
2024-02-23 21:33:44 -0600

    No more tie weights, instead treat transpose of lm head as embeddings table

--- TALNT.py
+++ TALNT.py
@@ -1,5 +1,6 @@
 import logging
 import torch
+import torch.nn.functional as F
 
 # This should work for any HuggingFace transformers model and tokenizer
 
@@ -19,12 +20,19 @@ def add_token(model, tokenizer, token, description):
     description_tokens = torch.tensor(tokenizer(description)["input_ids"])
     embeddings_sum = new_token_embeddings(description_tokens).sum(dim=0)
 
-    # Set the new token's embedding to the sum of the description's token embeddings and tie weights
+    # Do the same for the LM head
+    new_lm_head = model.get_output_embeddings()
+    weights_t = new_lm_head.weight.t()
+    lm_head_embeddings_sum = F.embedding(
+        description_tokens, weights_t).sum(dim=0)
+
+    # Set the new token's embedding to the sum of the description's token embeddings
     new_token_embeddings_module = model.get_input_embeddings()
     with torch.no_grad():
         new_token_embeddings_module.weight[-1, :] = embeddings_sum
+        new_lm_head.weight[:, -1] = lm_head_embeddings_sum
     model.set_input_embeddings(new_token_embeddings)
-    model.tie_weights()
+    model.set_output_embeddings(new_lm_head)
 
     return model, tokenizer
 
